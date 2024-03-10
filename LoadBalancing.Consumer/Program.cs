3e858658e5a91b60fc040af657f9ae5ad1347119
Ali Majidi <seyedalimajidi76@gmail.com>
2024-03-10 23:45:16 +0330

    Add project files.

---
+++ LoadBalancing.Consumer/Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+using var connection = new ConnectionFactory(){HostName = "localhost"}.CreateConnection();
+using var channel = connection.CreateModel();
+var queueName="LoadBalance";
+channel.QueueDeclare(queueName,true,false,false);
+
+var consumer = new EventingBasicConsumer(channel);
+
+consumer.Received += Consumer_Received;
+
+channel.BasicConsume(queueName, false, consumer);
+void Consumer_Received(object? sender, BasicDeliverEventArgs e)
+{
+
+    var message = System.Text.Encoding.UTF8.GetString(e.Body.ToArray());
+    System.Threading.Thread.Sleep(2000);
+    Console.WriteLine($"[+] new message :" + message);
+    channel.BasicAck(e.DeliveryTag)=false;
+    
+}
+
+Console.WriteLine("Press button to exit program");
+Console.ReadLine();
---
+++ LoadBalancing.Producer/Program.cs
@@ -0,0 +1,21 @@
+﻿
+using RabbitMQ.Client;
+
+using var connection = new ConnectionFactory(){HostName = "localhost"}.CreateConnection();
+using var channel = connection.CreateModel();
+var queueName = "LoadBalance";
+channel.QueueDeclare(queueName,true,false,false);
+Console.WriteLine("Please write your message and Press [ENTER] : \n");
+var msg = Console.ReadLine();
+var msgprop = channel.CreateBasicProperties();
+msgprop.Persistent = true;
+for (int i = 0; i < 20; i++)
+{
+    var message =$"{i}" + msg ;
+    var messageBinary = System.Text.Encoding.UTF8.GetBytes(message).ToArray();
+    channel.BasicPublish("", queueName, null, messageBinary);
+}
+
+Console.WriteLine("your message publish successed");
+Console.WriteLine("Press button to exit program");
+Console.ReadLine();
